6414f2d364e0fd4a3018e61bafbc22287069dd8f
matchre <mrchretien@gmail.com>
2019-03-07 15:44:18 +0100

    autopush

--- autopush.bat
+++ autopush.bat
@@ -1,31 +1,27 @@
-:loop
-	
 	:: Navigate to the directory you wish to push to GitHub
 	::Change <path> as needed. Eg. C:\Users\pookie\Desktop\Writings
-	cd D:\Documents\github
-	
+	::cd D:\Documents\github
+	TIMEOUT 1
 	::Initialize GitHub
 	git init
-	
+	TIMEOUT 1
 	::Pull any external changes (maybe you deleted a file from your repo?)
 	git pull
-	
+	TIMEOUT 1
 	::Add all files in the directory
 	git add --all
-	
+	TIMEOUT 1
 	::Commit all changes with the message "auto push". 
 	::Change as needed.
 	git commit -m "auto push"
-	
+	TIMEOUT 1
 	::Push all changes to GitHub 
 	git push
-	
+	TIMEOUT 1
 	::Alert user to script completion and relaunch.
 	echo Complete. Relaunching...
 	
 	::Wait 300 seconds until going to the start of the loop.
 	::Change as needed.
-	TIMEOUT 10
+	TIMEOUT 1
 	
-::Restart from the top.	
-goto loop
